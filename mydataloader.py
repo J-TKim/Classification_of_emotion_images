5d4511005bbde976a0337ef81df25026b5311613
J-TKim <kkkd991020@naver.com>
2020-11-25 03:13:57 +0000

    Upload files

--- mydataloader.py
+++ mydataloader.py
@@ -10,20 +10,21 @@ def data_loader(train_data, test_data = None , test_size= None, batch_size = 32)
         
         data_len = len(train_data)
         indices = list(range(data_len))
-        #np.random.shuffle(indices)
+        np.random.shuffle(indices)
         split1 = int(np.floor(0.2 * data_len))
         valid_idx , test_idx = indices[:split1], indices[split1:]
         test_sampler = SubsetRandomSampler(valid_idx)
         test_loader = DataLoader(train_data, batch_size= batch_size, sampler=test_sampler)
         dataloaders = {'train':train_loader,'test':test_loader}
         
-        ###################
+        
         kfold = []
 
         for i in range(5):
             data = train_loader
             data_len = len(data)
             indices = list(range(data_len))
+            np.random.shuffle(indices)
             split2 = int(np.floor(0.25 * data_len))
             #train1_idx, valid_idx, train2_idx = indices[:split2 * i], indices[split2 * i : split2 * (i+1)], indices[split2 * (i+1):]
             valid_idx= indices[split2 * i : split2 * (i+1)]
